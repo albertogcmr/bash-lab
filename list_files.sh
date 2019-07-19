5aea21d9e79f71c3062b3e10134e6ecd00f926e5
Pela12345 <pelayoav@gmail.com>
2019-07-19 18:02:31 +0200

    adding file sh

---
+++ list_files.sh
@@ -0,0 +1,20 @@
+1. echo Hello World
+2. mkdir new_dir
+3. rmdir new_dir
+4. cp lorem/sed.txt lorem-copy/sed.txt
+5. cp lorem/at.txt lorem/lorem.txt ./lorem-copy
+6. cat lorem/sed.txt
+7. cat lorem/at.txt lorem/lorem.txt
+8. head -3 lorem-copy/sed.txt
+9. tail -3 lorem-copy/sed.txt
+10. echo Homo homini lupus. >> lorem-copy/sed.txt
+11. tail -3 lorem-copy/sed.txt
+12. sed 's/et/ET/g' lorem-copy/sed.txt
+13. whoami
+14. pwd
+15. find lorem -name "*.txt"
+16. cat lorem/sed.txt | wc -l
+17. find * -name "lorem*" | wc -l
+18. grep et lorem/at.txt
+19. grep et lorem/at.txt | wc -l
+20. grep et lorem-copy/* | wc -l
