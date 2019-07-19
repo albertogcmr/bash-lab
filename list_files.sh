fe43766872e1075eb34b9e443aaaeb1a709a074c
delia <deliaclarramirez@gmail.com>
2019-07-19 17:15:59 +0200

    hecho

---
+++ list_files.sh
@@ -0,0 +1,20 @@
+banner hello world
+mkdir new_dir
+rm -r new_dir
+cp lorem/sed.txt lorem-copy/sed.txt
+cp -r lorem/ lorem-copy
+cat lorem/sed.txt
+cat lorem/at.txt lorem/lorem.txt
+head -n 3 lorem-copy/sed.txt
+tail -3 lorem-copy/sed.txt
+echo "Homo homini lupus" >> lorem-copy/sed.txt
+tail -3 lorem-copy/sed.txt
+sed -i '' 's/et/ET/g' lorem/at.txt
+who
+
+find lorem -iname "*.txt"
+cat lorem/sed.txt | wc -l
+find . -name 'lorem*' | wc -l
+grep -i "et" lorem/at.txt
+grep -i -r "et" lorem/at.txt | wc -l
+grep -i -r "et" lorem-copy/ | wc -l
