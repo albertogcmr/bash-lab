72e019142d5b7cb7d617843bc5ecb165f76dce50
lgarciaco1 <lcobaleda1@gmail.com>
2019-07-22 09:38:27 +0200

    Done

---
+++ list_files.sh
@@ -0,0 +1,39 @@
+1) banner Hello world
+
+2) mkdir new_dir
+
+3) -r new_dir
+
+4) cp lorem/sed.txt lorem-copy/sed.txt
+
+5) cp -r lorem/ lorem-copy
+
+6) cat lorem/sed.txt
+
+7) cat lorem/at.txt lorem/lorem.txt
+
+8) head -n 3 lorem-copy/sed.txt
+
+9) tail -3 lorem-copy/sed.txt
+
+10) echo "Homo homini lupus" >> lorem-copy/sed.txt
+
+11) tail -3 lorem-copy/sed.txt
+
+12) sed -i "" "s/et/ET/g" lorem/at.txt
+
+13) who
+
+14)
+
+15) find lorem -iname "*.txt"
+
+16) cat lorem/sed.txt | wc -l
+
+17) find . -name "lorem*" | wc -l
+
+18) grep -i "et" lorem/at.txt
+
+19) grep -i "et" lorem/at.txt | wc -l
+
+20) grep -i -r "et" lorem-copy/ | wc -l
