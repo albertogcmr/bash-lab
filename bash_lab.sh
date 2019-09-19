8893ec3887106bd6aa5d278d3f6fb2d92e57d928
Ines P. Chavarri <inespchavarri@gmail.com>
2019-09-19 17:14:17 +0200

    Ejercicio casi terminados

--- bash_lab.sh
+++ bash_lab.sh
@@ -1,5 +1,99 @@
+# Imprime en consola "Hello World".
+
 echo "Hello World"
+
+# Crea un directorio nuevo llamado new_dir
 
 mkdir new_dir
 
-rm new_dir
+# Elimina ese directorio.
+
+rm -r new_dir
+
+# Copia el archivo sed.txt dentro de la carpeta lorem a la carpeta lorem-copy.
+
+cp lorem/sed.txt lorem-copy/
+
+# Copia los otros dos archivos de la carpeta lorem a la carpeta lorem-copy en una sola linea mediante ;
+
+cp lorem/at.txt lorem/lorem.txt lorem-copy/
+
+# Muestra el contenido del archivo sed.txt dentro de la carpeta lorem
+
+cat lorem/sed.txt
+
+# Muestra el contenido de los archivos at.txt y lorem.txt dentro de la carpeta lorem
+
+cat lorem/at.txt lorem/lorem.txt
+
+# Visualiza las primeras 3 linas del archivo sed.txt dentro de la carpeta lorem-copy
+
+head -3 lorem-copy/sed.txt
+
+# Visualiza las ultimas 3 linas del archivo sed.txt dentro de la carpeta lorem-copy
+
+tail -3 lorem-copy/sed.txt
+
+# Añade Homo homini lupus. al final de archivo sed.txt dentro de la carpeta lorem-copy.
+
+echo "Homo homini lupus." >> lorem-copy/sed.txt
+
+
+# Visualiza las últimas 3 linas del archivo sed.txt dentro de la carpeta lorem-copy. Deberías ver ahora Homo homini lupus..
+
+tail -3 lorem-copy/sed.txt
+
+# Sustituye todas las apariciones de et por ET del archivo at.txt dentro de la carpeta lorem-copy. Deberás usar sed
+
+sed -i "" "s/et/ET/g" lorem-copy/at.txt
+
+# Encuentra al usuario activo en el sistema.
+
+id -un
+
+# Encuentra dónde estás en tu sistema de ficheros.
+
+pwd
+
+# Lista los archivos que terminan por .txt en la carpeta lorem.
+
+find lorem -type f -name "*.txt"
+
+# Cuenta el número de lineas que tiene el archivo sed.txt dentro de la carpeta lorem. Tendrás que encadenar cat y wc mediante el pipe |.
+
+cat lorem/sed.txt | wc -l
+
+# Cuenta el número de archivos que empiezan por lorem que están en este directorio y en directorios internos
+
+find . -type f -name "lorem*" | wc -l
+
+# Encuentra todas las apariciones de et en at.txt dentro de la carpeta lorem.
+
+
+
+# Cuenta el número de apariciones del string et en at.txt dentro de la carpeta lorem. Para ello debes obtener sólo los string buscados y contar las lineas.
+
+
+
+# Cuenta el número de apariciones del string et en todos los archivos del directorio lorem-copy.
+
+
+
+# BONUS
+
+
+# Almacena en una variable name tu nombre mediante el comando read.
+
+
+# Imprime esa variable.
+
+
+# Crea un directorio nuevo que se llame como el contenido de la variable name.
+
+
+# Elimina ese directorio.
+
+
+# Por cada archivo dentro de la carpeta lorem imprime el número de carácteres que tienen sus nombres. 
+# Intenta primero mostrar los archivos mediante un bucle for. Luego calcula la longitud de cada elemento de la iteración.
+# Imprime los ficheros / Imprime las longitudes de los nombres de los ficheros.
